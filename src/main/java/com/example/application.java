6e2f0cd0ffb9854c6ed35aa6e8acf39873729d58
viyanikurian-commits <viyanikurian@gmail.com>
2026-03-18 12:33:12 +0530

    Create application.java

---
+++ src/main/java/com/example/application.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello from OpenShift 🚀";
+    }
+}
